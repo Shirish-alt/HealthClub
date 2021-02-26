73fc4cc27c5044c48a0ab0a10aaeb8a9a909b0ed
vbadheli <vbadheli@gmail.com>
2021-02-26 15:28:28 +0530

    Profile page design created

--- app/src/main/java/com/example/healthclub_app/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/healthclub_app/Activities/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.healthclub_app.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
@@ -13,7 +14,7 @@ import com.example.healthclub_app.MainActivity;
 import com.example.healthclub_app.R;
 import com.google.android.material.tabs.TabLayout;
 
-public class Progress extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     String access_token;
     TabLayout tabLayout;
@@ -22,18 +23,19 @@ public class Progress extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+//        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
+//        viewPager=(ViewPager) findViewById(R.id.progress_viewPager);
 
-        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
-        viewPager=(ViewPager) findViewById(R.id.progress_viewPager);
+//        tabLayout.addTab(tabLayout.newTab().setText("Graph"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tablular"));
 
-        tabLayout.addTab(tabLayout.newTab().setText("Graph"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tablular"));
-
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         access_token=getIntent().getStringExtra("Token");
         if(access_token==null){
-            Toast.makeText(this, "Plaes Login", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Please Login", Toast.LENGTH_SHORT).show();
         }else
         {
             Log.e("Toekn:",""+access_token);
@@ -41,7 +43,7 @@ public class Progress extends AppCompatActivity {
         }
 
 
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        /*tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
         final TableTabAdapter adapter = new TableTabAdapter(this,getSupportFragmentManager(), tabLayout.getTabCount(),access_token);
         viewPager.setAdapter(adapter);
@@ -62,12 +64,12 @@ public class Progress extends AppCompatActivity {
             public void onTabReselected(TabLayout.Tab tab) {
 
             }
-        });
+        });*/
 
     }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
