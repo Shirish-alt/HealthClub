9911ff09e66e0373c02e156ddf4b926b9952e935
vbadheli <vbadheli@gmail.com>
2021-03-04 11:31:01 +0530

    this test

--- app/src/main/java/com/example/healthclub_app/Activities/FullScreen.java
+++ app/src/main/java/com/example/healthclub_app/Activities/FullScreen.java
@@ -34,9 +34,9 @@ public class FullScreen extends YouTubeBaseActivity{
                 if(!b){
                     youTubePlayer.loadVideo( url );
 
-                    youTubePlayer.setPlayerStyle( YouTubePlayer.PlayerStyle.DEFAULT );
-                    youTubePlayer.setFullscreen(true);
-                    youTubePlayer.play();
+//                    youTubePlayer.setPlayerStyle( YouTubePlayer.PlayerStyle.DEFAULT );
+//                    youTubePlayer.setFullscreen(true);
+//                    youTubePlayer.play();
 
                 }
             }
